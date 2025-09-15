5255f58e713db7245b431521f531ac6c09b86d9d
crymes8 <cameronraymes@gmail.com>
2025-09-15 11:20:15 -0400

    Update cleanup.sql

--- cleanup.sql
+++ cleanup.sql
@@ -2,11 +2,18 @@ USE ROLE accountadmin;
 USE WAREHOUSE compute_wh;
 
 USE DATABASE tasty_bytes;
-DROP VIEW IF EXISTS tasty_bytes.harmonized.daily_weather_v;
-DROP VIEW IF EXISTS tasty_bytes.harmonized.windspeed_hamburg;
-DROP FUNCTION IF EXISTS tasty_bytes.analytics.fahrenheit_to_celsius();
-DROP FUNCTION IF EXISTS tasty_bytes.analytics.inch_to_millimeter();
-DROP VIEW IF EXISTS tasty_bytes.harmonized.weather_hamburg;
-DROP VIEW IF EXISTS tasty_bytes.analytics.daily_city_metrics_v;
-DROP STREAM IF EXISTS tasty_bytes.raw_pos.order_header_stream;
-DROP PROCEDURE IF EXISTS tasty_bytes.raw_pos.process_order_headers_stream();
+DROP VIEW tasty_bytes.harmonized.daily_weather_v;
+
+DROP VIEW tasty_bytes.harmonized.windspeed_hamburg;
+
+DROP FUNCTION tasty_bytes.analytics.fahrenheit_to_celsius();
+
+DROP FUNCTION tasty_bytes.analytics.inch_to_millimeter();
+
+DROP VIEW tasty_bytes.harmonized.weather_hamburg;
+
+DROP VIEW tasty_bytes.analytics.daily_city_metrics_v;
+
+DROP STREAM tasty_bytes.raw_pos.order_header_stream ON TABLE tasty_bytes.raw_pos.order_header;
+
+DROP PROCEDURE tasty_bytes.raw_pos.process_order_headers_stream();
